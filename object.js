1bf33cfd384bcb676ba89697442fcebb6ff74231
sneha-d2004 <dokkasneha83@gmail.com>
2025-10-27 23:16:25 +0530

    js

---
+++ object.js
@@ -0,0 +1,22 @@
+var car={
+    name:"benz",
+    price:2000000,
+    color:"black",
+    weight:100,
+
+}
+console.log(car)
+
+var van={name:"van",price:100000,color:"white",}
+console.log(van)
+
+
+var person={name:"sneha",age:21,height:5.4}
+console.log(person)
+
+var person1={name:"prashanth",age:29}
+var person2={height:6,weight:50}
+console.log(person1);
+console.log(person2);
+var person3={...person1,...person2}
+console.log(person3)
