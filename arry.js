4299c062cb8587d100585723e2572e6dd62c2d33
sneha-d2004 <dokkasneha83@gmail.com>
2025-10-27 23:15:03 +0530

    Add files via upload

---
+++ arry.js
@@ -0,0 +1,16 @@
+var arr=["sneha","prashant",21,6.0,true];
+console.log(arr);
+arr[0]="motupalli"
+console.log(arr);
+arr.push("male")
+console.log(arr);
+arr.pop();
+console.log(arr);
+arr.unshift("male")
+console.log(arr);
+arr.shift();
+console.log(arr);
+var index=arr.indexOf(21)
+console.log(index);
+console.log(arr.length);
+
